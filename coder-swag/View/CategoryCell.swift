379a6038e232226c4c72206ff30a2b57b0798fc5
Cory Braun <corybraun@Corys-MacBook.local>
2018-06-21 17:08:43 -0700

    Setting up UI

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Cory Braun on 6/21/18.
+//  Copyright © 2018 Cory Braun. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
